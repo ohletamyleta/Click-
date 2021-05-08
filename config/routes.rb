ed20192c1af1aae5818415b12aab21bcb1eb8522
ohletamyleta <ohletamyleta@gmail.com>
2021-05-08 10:52:06 -0400

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
+root 'sessions#index'
+ 
+ get '/login' => 'sessions#new'
+ post '/login' => 'sessions#create'
+ get '/logout' => 'sessions#destroy'
+
+ get '/signup' => 'users#new'
+ post '/signup' => 'users#create'
+
   resources :photos
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
